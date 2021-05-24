a80d69927275595a3430c85480737651fbb49ee7
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-05-24 10:25:10 +0530

    Add files via upload

---
+++ ATM2.py
@@ -0,0 +1,18 @@
+import math
+test=1
+test=int(input())
+
+for tc in range((test)):
+        #Write your code....
+        n,k=map(int,input().split())
+        l=list(map(int,input().split()))
+        res=[]
+        for i in l:
+                if i<=k:
+                        res.append(1)
+                        k-=i
+                else:
+                        res.append(0)
+        for i in res:
+                print(i,sep='', end='')
+        print()
