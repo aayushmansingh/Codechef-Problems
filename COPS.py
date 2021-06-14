d925537e385e72ac28603e1f35093e537ab02d34
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-06-14 11:06:03 +0530

    Add files via upload

---
+++ COPS.py
@@ -0,0 +1,16 @@
+for tc in range(int(input())):
+    m,x,y=list(map(int,input().split()))
+    loc=list(map(int,input().split()))
+    product=x*y
+    ans=[]
+    for house in loc:
+        a=house+product
+        b=house-product
+        if a>100:
+            a=100
+        if b<1:
+            b=1
+        for i in range(b,a+1):
+            ans.append(i)
+    ans=set(ans)
+    print(100-len(ans))
