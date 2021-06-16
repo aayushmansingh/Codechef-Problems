e63ec2dd6949909c2ec005e454e33a83ae2323fc
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-06-16 11:13:05 +0530

    Add files via upload

---
+++ Chef_On_Island.py
@@ -0,0 +1,12 @@
+import math
+test = 1
+test = int(input())
+
+for tc in range((test)):
+    # Write your code....
+    x, y, xr, yr, d = map(int, input().split())
+    res = min(x / xr, y / yr)
+    if res >= d:
+        print("YES")
+    else:
+        print("NO")
